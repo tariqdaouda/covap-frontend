270a55c2e912458b262c152918d66bf4e5e9daf3
tariqdaouda <tariq@sauropod.com>
2020-04-06 03:17:32 -0400

    text update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -16,7 +16,7 @@
                         These are preliminary results
                     </p>
                     <p class="we-abstract-p">
-                        Publishing scientific findings takes months. Given the actual situation, we have decided to make our results freely available now. To both the research community and the general public.
+                        Publishing scientific findings takes months. Given the urgency of the situation, we have decided to make our results freely available now. To both the research community and the general public.
                     </p>
                 </div>
                 <div class="uk-width-1-2@m uk-width-1-1@s uk-text-center uk-margin-auto uk-margin-auto-vertical">

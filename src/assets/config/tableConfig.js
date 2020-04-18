c986849ff9e8a073d11a1c812ebbcea10f994dc9
Logan Schwartz <loganschwartz@hotmail.com>
2020-04-18 16:41:39 -0400

    First table proto with one remaining bug

--- src/assets/config/tableConfig.js
+++ src/assets/config/tableConfig.js
@@ -1,327 +1,37 @@
 const tableColumnsConfig = [
-  // TODO move list to a config file
   {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Index",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Index",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Method",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Method",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Position",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Position",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Model Run",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Model_run",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Context Size",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Context_size",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Score",
-    "search": false,
-    "hidden": false,
+    "title": "Score",
     "field": "Peptides.Score",
-    "smallField": false
+    "sortable": true,
+    "visible": true,
   },
   {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Sub Accession",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Sub_accession",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Length",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Length",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Accession",
-    "search": false,
-    "hidden": false,
-    "field": "Peptides.Accession",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Peptides Sequence",
-    "search": false,
-    "hidden": false,
+    "title": "Sequence",
     "field": "Peptides.Sequence",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Release Date",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Release_Date",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Genome Region",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Genome_Region",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Isolation Source",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Isolation_Source",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Version",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Version",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Index",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Index",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Accession",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Accession",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Authors",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Authors",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Host",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Host",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences BioSample",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.BioSample",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Segment",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Segment",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Nuc_Completeness",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Nuc_Completeness",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Sequence",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Sequence",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Family",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Family",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Collection Date",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Collection_Date",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Sub Accession",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Sub_accession",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Length",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Length",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Protein Accession",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Protein_accession",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Publications",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Publications",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Geo Location",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Geo_Location",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences GenBank Title",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.GenBank_Title",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Genotype",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Genotype",
-    "smallField": false
-  },
-  {
-    "sortable": false,
-    "filter": false,
-    "type": "",
-    "title": "Virus Sequences Genus",
-    "search": false,
-    "hidden": false,
-    "field": "VirusSequences.Genus",
-    "smallField": false
+    "sortable": true,
+    "visible": true,
   }
 ];
 
-const tableRowPerPageConfig = [50,500,5000];
+// default column title used to sort result and order
+const sortColumn = "Score";
+const sortOrder = "descending";
 
-export default { tableColumnsConfig, tableRowPerPageConfig };
+// API max returned results limit
+const maxRows = 5000;
+
+// Max displayed pages at a time
+// before showing up ...
+const maxDisplayedPages = 6;
+
+// Rows per page options values
+const tableRowPerPageConfig = [1,50,500,5000];
+
+export default { 
+  tableColumnsConfig,
+  tableRowPerPageConfig,
+  maxRows,
+  sortColumn,
+  sortOrder,
+  maxDisplayedPages
+};
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,61 +1,102 @@
 <template>
+  <div class="uk-text-center uk-margin-auto">
 
-  <div>
-    
-    <!-- Column visibility button + modal-->
-    <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-example">Hide/Show Columns</button>
-    <div id="modal-example" uk-modal>
+    <div>
+      <!-- options buttons-->
+      <!-- Column visibility button + modal-->
+      <button class="uk-button uk-button-default uk-margin-small-right uk-button-primary" type="button" uk-toggle="target: #modal-columnOptions">Hide/Show Columns</button>
+      <div id="modal-columnOptions" uk-modal>
         <div class="uk-modal-dialog uk-modal-body">
-            <h2 class="uk-modal-title">Visible Columns:</h2>
-            <ul>
-              <li v-for="column in columns"  v-bind:key="column">
-                <!--There is a bug here checkbox should be checked by default as hidden is set to false-->
-                <input type="checkbox" v-model="column.hidden" ><label>{{column.title}}</label> 
-              </li>
-            </ul>
-            <p class="uk-text-right">
-                <button class="uk-button uk-button-default uk-modal-close" type="button">Exit</button>
-            </p>
+          <h5 class="uk-modal-header">Visible Columns:</h5>
+          <ul class="uk-text-center">
+            <li v-for="(column, index) in tableColumnsConfig" v-bind:key="index" id="index" style="list-style-type: none;">
+              <input type="checkbox" v-model="column.visible" ><label>{{column.title}}</label> 
+            </li>
+          </ul>
+          <p class="uk-text-right">
+            <button class="uk-button uk-button-default uk-modal-close" type="button">Exit</button>
+          </p>
         </div>
+      </div>
+
+      <!--Export table-->
+      <button class="uk-button uk-button-default uk-margin-small-right uk-button-primary" type="button" v-on:click="downloadCSV()">Export Table To CSV</button>
     </div>
 
-    <!--Export table to CSV-->
-    <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-example">Export to CSV</button>
-    
+    <!--Max Row warning-->
+    <div class="uk-alert-warning" uk-alert v-show="rows.length === maxRows">
+        <p>
+          <span uk-icon="warning"></span>
+          Your query was too broad and has been capped to {{maxRows}} results.
+        </p>
+    </div>
+    {{rows}}
+
+
     <!--Data Table-->
-    <!--
-    columns are being retrieved from config file at '@/assets/config/tableColumnsConfig.js'
-    rows is the json data returned by the api
-    per_page is binded to rowsPerPage that is the selected value of the combobox above
-    -->
-    <vk-datatable
-        :columns="columns"
-        :rows="rows"
-        :per_page="rowsPerPage"
-        defaultSort="Peptides.Score"
-        >
-    </vk-datatable>
+    <div class="uk-row uk-flex-center">
+      <table class="uk-table uk-overflow-container">
+        <thead>
+          <tr>
+            <th v-for="(column, index) in columns" v-bind:key="index" class="uk-text-center" v-on:click="sortTable(column)">
+              {{column.title}}
+              <span uk-icon="icon: triangle-down;" v-show=" column.sortable && column.title == sortColumn && sortOrder === 'descending'"></span>
+              <span uk-icon="icon: triangle-up;" v-show="column.sortable && column.title == sortColumn && sortOrder === 'ascending'"></span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in dataTableRange" v-bind:key="row">
+            <td v-for="column in columns" v-bind:key="column">
+              {{row[column.field]}}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
 
     <!--Pagination-->
-    <!--
-      With the cursor batch query we can't know the amount of pages that may be returned
-      The only way to get this information would be to do query sorted without limit args and returns 
-      all rows associated in a range like 0-500 for page 1 and so on...
-    -->
-    <div>
-      <ul class="uk-pagination" uk-margin>
-          <li><a href="#"><span uk-pagination-previous></span></a></li>
-          <li v-for="page in pageCount" v-bind:key="page" v-bind:class="{ 'uk-active': page === currentPage }"><a href="#">{{page}}</a></li>
-          <li><a href="#"><span uk-pagination-next></span></a></li>
-      </ul>
+    <div class="uk-flex-center">
+      <ul class="uk-pagination uk-flex-center" uk-margin>
+        <!--Previous-->
+        <li>
+          <a @click="updateSelectedPage(currentPage-1)" v-bind:class="{ 'uk-disabled': currentPage === 1 }">
+            <span uk-pagination-previous></span>
+          </a>
+        </li>
 
-      <div>
-          Rows per page: <multiselect v-model="selectedRowPerPageValue" :options="rowsPerPageOptions" @change="updateSelectedRowPerPageValue"></multiselect>       
-      </div>
-      {{getDataTableRange}}
+        <li class="uk-disabled" v-show="currentPage - maxDisplayedPages/2 > 1"><span>...</span></li>
 
+        <!--Pages-->
+        <li v-for="idx in paginationList" v-bind:key="idx"  >
+          <a @click="updateSelectedPage(idx+1)" v-bind:class="{ 'uk-text-bolder': idx+1 === currentPage , 'uk-text-primary': idx+1 === currentPage}">
+            {{idx+1}}
+          </a>
+        </li>
+
+        <li class="uk-disabled"  v-show="currentPage + maxDisplayedPages/2 < pageCount"><span>...</span></li>
+
+        <!--Next-->
+        <li>
+          <a @click="updateSelectedPage(currentPage+1)" v-bind:class="{ 'uk-disabled': currentPage === pageCount }">
+            <span uk-pagination-next></span>
+          </a>
+        </li>
+      </ul> 
+
+      <div class="uk-row">
+        <p>
+          Rows Per Page:
+          <span>
+            <multiselect 
+              v-model="selectedRowPerPageValue"
+              :options="rowsPerPageOptions">
+            </multiselect>
+          </span>
+        </p>
+      </div>     
     </div>
-
   </div>
 </template>
 
@@ -63,106 +104,125 @@
 <script>
   import config from '@/assets/config/tableConfig.js'
   import Multiselect from 'vue-multiselect'
-  import VuiKitDatatable from 'vuikit-datatable'
-  import Vue from 'vue'
-
-  Vue.component('vk-datatable', VuiKitDatatable)
-  Vue.component('Multiselect', Multiselect)
 
   export default {
     name: 'TableComponent',
+    components: {
+      Multiselect
+    },
+    props: ['rows'],
     data() {
       return {
+
+          // original column order
+          tableColumnsConfig: null,
+
           // pagination
-          pageCount: 1,
-          currentPage: 1,
-          selectedRowPerPageValue: 0,
+          currentPage: null,
+          selectedRowPerPageValue: null,
           rowsPerPageOptions: [],
-          // table props
-          columns: [],
-          rows: [],
-          rowsPerPage: 5,
-          loadingPropData: false,
-          selectableRows: true
+          maxRows: null,
+          maxDisplayedPages: null, // pagination
+
+          // sort
+          sortColumn: null,
+          sortOrder: null
         }
     },
     computed: {
-      getDataTableRange: function () {
-        let lowerRange = (this.pageCount - 1) * this.selectedRowPerPageValue;
+      columns: function() {
+        return this.tableColumnsConfig ? this.tableColumnsConfig.filter( function(elt) { return elt.visible }) : [];
+      },
+      pageCount: function(){
+        return Math.ceil(this.rows.length/this.selectedRowPerPageValue);
+      },
+      paginationList: function () {
+        let lowEnd = this.currentPage - this.maxDisplayedPages/2 > 0 ? Math.floor(this.currentPage - this.maxDisplayedPages/2) - 1 : 0;
+        let highEnd = this.currentPage + this.maxDisplayedPages/2 < this.pageCount ? Math.ceil(this.currentPage + this.maxDisplayedPages/2) : this.pageCount;
+        
+        var list = [];
+        for (var i = lowEnd; i < highEnd; i++) {
+            list.push(i);
+        }
+
+        return list;
+      },
+      dataTableRange: function () {
+        let lowerRange = (this.currentPage - 1) * this.selectedRowPerPageValue;
         let higherRange = lowerRange + this.selectedRowPerPageValue;
-        return [lowerRange, higherRange]
+        return this.rows.slice(lowerRange, higherRange)
       }
     },
     methods: {
-      updateSelectedRowPerPageValue: function() {
-        console.log('updateSelectedRowPerPageValue', this.selectedRowPerPageValue);
+      updateSelectedPage: function(index) {
+        if (index != 0 &&  index != this.pageCount + 1) {
+          this.currentPage = index;
+        }
+      },
+      sortTable: function(column) {
+        // reverse sort order if column is already
+        // the sort column else sort normaly
+        if (column.sortable === false) {
+          return 0
+        } else if (this.sortColumn === column.title) {
+          this.sortOrder = this.sortOrder === "descending" ? "ascending" : "descending";
+        } else {
+          this.sortOrder = "descending";
+          this.sortColumn = column.title;
+        }
+
+        // sort rows
+        var sortOrder = this.sortOrder;
+        this.rows.sort(function(a, b) {
+            if (a[column.field] > b[column.field]) {
+              return sortOrder === "ascending" ? 1 : -1
+            } else if (a[column.field] < b[column.field]) {
+              return sortOrder === "ascending" ? -1 : 1
+            }
+            return 0;
+          }
+        )
+      },
+      JsonToCSV: function (){
+        
+        // create columns row
+        var csvStr = this.columns.map(function(elt){return elt.title}).join(",") + "\n";
+
+        // iterate over each rows and populate csv
+        this.rows.forEach(row => {
+            this.columns.forEach(column => {
+              csvStr += row[column.field] + ','
+            })
+            csvStr += "\n";
+          })
+        return csvStr;
+      },
+      downloadCSV: function () {
+        let hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(this.JsonToCSV());
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'epitopes.csv';
+        hiddenElement.click();
       }
     },
     mounted: function() {
-      this.columns = config.tableColumnsConfig;
+
+      // storing config to be able
+      // to reset it later on if needed
+      this.tableColumnsConfig = config.tableColumnsConfig;
+
+      // pagination
+      this.maxDisplayedPages = config.maxDisplayedPages; 
       this.rowsPerPageOptions = config.tableRowPerPageConfig;
       this.selectedRowPerPageValue = config.tableRowPerPageConfig[0];
+      this.updateSelectedPage(1);
+      this.maxRows = config.maxRows;
+
+      // default sort column/order
+      this.sortColumn = config.sortColumn;
+      this.sortOrder =  config.sortOrder;
     }
 }
-
 </script>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
-
-/*
-
-  <!--
-  <vk-datatable
-      :columns="columns"
-      :rows="data"
-      :per_page="5"
-      :actions="actions"
-      v-on:row_click="rowClick"
-      v-on:row_delete="rowDelete"
-      v-on:row_edit="rowEdit"
-      defaultSort="" // the default column to sort by
-      loadingPropData="" // boolean to show table loader
-      selectableRows="" // multiple rows can be selected
-      v-on:rowsSelected="" // callback for when a row is selected
-      >
-  </vk-datatable>
-  -->
-          // input format for column
-          columns: [
-              {
-                title: '', // The title of the column
-                field: '', // the field name in the data array\
-                hidden: true|false , // hide this field
-                search: true|false,  // is field searchable
-                filter: true|false , // is filtererable
-                sortable: true|false,  // field is sortable
-                type: '', // field type, see bellow for options
-                prefix:'', // text before the value in the cell
-                suffix: '' // text after the value in the cell
-                inputsettings: {} // takes values used for number and text input fields,
-                onchange: function (row) {}, // callback for input field value changes
-                smallField: true|false // if true will add uk-table-shrink to the column
-              },
-          ],
-          // input format for data
-          data: [],
-          // define actions for row
-          actions: [
-              {title: 'Edit Post', event: 'edit', icon: 'pencil'}, // creates an row_edit event
-              {title: 'Delete Post', event: 'delete', icon: 'trash'}, // creates an row_delete event
-          ],
-
-Available column types are
-VALUE   Description
-''  Text only field
-'array_implode'   Imploded array of values comma seperated
-'date_diff'   uses moment differece for humans to return the difference between the given data and today
-'number'  numeric field used for sorting
-'boolean'   Turns a true or false value into a yes or no
-'image_url'   Shows a 30x30px thumbnail
-'string_array'  Outputs an array of strings with each value on a new line
-'textinput'   Shows a text input and calls a callback on value change. available settings (placeholder)
-'numberinput'   Shows a numer input and calls a callback on value change. available settings (placeholder, min, max)
-
-*/
-

40d8e4e6eecfecee898f68b54c37da13b3535943
tariqdaouda <tariq@sauropod.com>
2020-04-18 03:33:16 -0400

    responsive lang switcher + bad fr translation

---
+++ src/components/NavLocaleSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+    <select class="we-locale-switcher" v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang" class="we-locale-switcher-item">{{ lang }}</option>
+    </select>
+</template>
+
+<script>
+    export default {
+      name: 'NavLocaleSwitch',
+      data () {
+        return { langs: ['en', 'fr'] }
+      }
+    }
+</script>

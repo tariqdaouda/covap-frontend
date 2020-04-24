f82e0bc547160f1813b63f60026aca432c08e90d
tariqdaouda <tariq@sauropod.com>
2020-04-24 14:50:55 -0400

    clean up

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="uk-text-center uk-align-center we-overlay">
-        <!-- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfNOQv3SFSp3n0u4EC8ig2q_ahhDSyADJXXQcV4DbzrznewQA/viewform?embedded=true" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">{{$t("loading")}}</iframe> -->
-         <iframe :src="forms[$i18n.locale]" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">{{$t("loading")}}</iframe>
+        <iframe :src="forms[$i18n.locale]" width="640" height="1000" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">{{$t("loading")}}</iframe>
     </div>
 </template>
 <script>
@@ -14,11 +13,6 @@
                 "en": "https://docs.google.com/forms/d/e/1FAIpQLSfNOQv3SFSp3n0u4EC8ig2q_ahhDSyADJXXQcV4DbzrznewQA/viewform?embedded=true",
                 "fr": "https://docs.google.com/forms/d/e/1FAIpQLSd6oj_3Be3mLuknaZCKFEPSZWbw4cB9kW3tOxVSomtwZ_D_iQ/viewform?embedded=true"
             }
-        }
-    },
-    methods:{
-        getFormUrl(){
-            
         }
     }
 }

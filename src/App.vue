00c47e53aab9d7be187d03f8cc132d49d45e4522
tariqdaouda <tariq@sauropod.com>
2020-04-28 13:52:51 -0400

    adedd image for social sharing in en

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,9 @@
           meta: [
             { charset: 'utf-8' },
             { description: 'We have developed an AI algorithm to help identify useful targets for a vaccine against COVID-19. Our goal is to accelerate the development of a vaccine.' },
+            { keywords: "COVID-19, SARS-CoV-2, COVID, coronavirus, epitopes, targets prediction, CAMAP, IRIC, immunotherapy, viral proteins, cancer, pandemic, virus, vaccines, machine learning, scientific, AI, algorithm"},
+            { "twitter:image": "../images/social_en.jpg" },
+            { "og:image": "../images/social_en.jpg" }
             // { vmid: 'description', name: 'description', content: 'reptilian' }
           ]
         }

3594df2417c0d0c958984daafa6256750e962cb1
tariqdaouda <tariq@sauropod.com>
2020-05-04 15:31:17 -0400

    added ui disclaimer

--- src/pages/Data.vue
+++ src/pages/Data.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+  import UIkit from 'uikit';
+  
   import DataComponent from '../components/DataComponent'
   export default {
     name: 'Data',
@@ -14,5 +16,9 @@
     components: {
       DataComponent,
     },
+    created(){
+      let html = "<div class='uk-text-center'><h2>Explore CAMAP results for SARS-CoV-2</h2><p>Unless stated otherwise Score refers to CAMAP score</p><p>This interface loads at most 5000 examples, distributions might change slighlty between queries</p></div>"
+      UIkit.modal.alert(html);
+    }
   }
 </script>
